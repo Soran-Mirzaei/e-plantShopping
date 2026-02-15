08751e6b803817c948bd921ba6d7471ae387c9c3
soran-mirzaei <soran.mirzaie@gmail.com>
2026-02-15 07:18:10 -0500

    modify store vite.config package

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import "./ProductList.css";
 import CartItem from "./CartItem";
 
 import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
+  const dispatch = useDispatch();
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addedToCart, setAddedToCart] = useState({});
@@ -300,6 +302,10 @@ function ProductList({ onHomeClick }) {
       [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
     }));
   };
+
+  const calculateTotalQuantity = () => {
+    return CartItem ? CartItem.reduce((total, item) => total + item.quantity, 0) : 0;
+     };
 
   return (
     <div>
